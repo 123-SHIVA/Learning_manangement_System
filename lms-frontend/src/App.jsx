b82011ac12f9ede2b862d1315b1d0dddaf7ee61e
123-SHIVA <shivayojha8@gmail.com>
2023-11-19 13:42:11 +0530

    eslint import sort added and react-toaster enable

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import {Routes,Route} from 'react-router-dom';
 import "./App.css";
 
 function App() {
-  return <>lms</>;
+  return <>
+          <Routes>
+            {/* <Route path='/' element={<Home/>}></Route> */}
+          </Routes>
+  </>;
 }
 
  
