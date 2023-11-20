4d9074dda0426adda58b8ea86af910a2655772bf
123-SHIVA <shivayojha8@gmail.com>
2023-11-20 13:23:24 +0530

    added footer component

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import {Routes,Route} from 'react-router-dom';
 import "./App.css";
+import Footer from './Components/Footer';
 
 function App() {
   return <>
-          <Routes>
-            {/* <Route path='/' element={<Home/>}></Route> */}
-          </Routes>
+          {/* <Routes>
+            <Route path='/' element={<Home/>}></Route>
+          </Routes> */}
+          <Footer/>
+
+
   </>;
 }
 
