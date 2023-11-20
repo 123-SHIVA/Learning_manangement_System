1043be3f9960898574f5d36fe30a72f46de83677
123-SHIVA <shivayojha8@gmail.com>
2023-11-20 12:04:56 +0530

    started adding basic redux setup for auth

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -4,13 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
 import Toaster from "react-hot-toast"
+import { Provider } from 'react-redux'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <Provider store={store}>
+ <BrowserRouter>
        <App />
        <Toaster/>
     </BrowserRouter>
+    </Provider>
+   
  
   
 )
